947cc50ff2ef3a6ef32be9e5ecc3b12137342003
POXS44 <vzeledon7@gmail.com>
2021-10-29 22:26:32 -0600

    Estem agregue la funcion de retirar dinero ademas de usarlas en FachadaCajero

--- src/main/java/org/poxs44/FACADE/FachadaCajero.java
+++ src/main/java/org/poxs44/FACADE/FachadaCajero.java
@@ -2,4 +2,20 @@ package org.poxs44.FACADE;
 
 public class FachadaCajero {
 
+    private ValidacionUsuario val = new ValidacionUsuario();
+    private RecargaMovil rec = new RecargaMovil();
+    private RetirarDinero ret = new RetirarDinero();
+
+
+
+    public String sacarDinero()
+    {
+        return val.valida() + ret.retirar();
+    }
+
+    public String recargaMovil()
+    {
+        return val.valida() + rec.recarga();
+    }
+
 }
--- src/main/java/org/poxs44/FACADE/RetirarDinero.java
+++ src/main/java/org/poxs44/FACADE/RetirarDinero.java
@@ -1,4 +1,12 @@
 package org.poxs44.FACADE;
 
 public class RetirarDinero {
+    public String retirar() {
+        String res;
+        res = "\n### Retirar Dinero:"
+                + "\n\tCuanto va ha retirar..."
+                + "\n\tProcesando..."
+                + "\n\tactualizando el saldo...";
+        return res;
+    }
 }
