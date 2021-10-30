17ac0dceea1a490452b81ec84015df3e78a36c1d
POXS44 <vzeledon7@gmail.com>
2021-10-29 22:46:37 -0600

    Ps ahi va todo  nice ya puede ingresar la opcion que quiera

--- src/main/java/org/poxs44/FACADE/PruebaPatron.java
+++ src/main/java/org/poxs44/FACADE/PruebaPatron.java
@@ -1,4 +1,27 @@
 package org.poxs44.FACADE;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class PruebaPatron {
+
+    public static void main(String args[]) throws IOException {
+
+        FachadaCajero fachada = new FachadaCajero();
+        BufferedReader entrada =new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Que queres " +
+                "\n1- Sacar Dinero " +
+                "\n2- Recarga Móvil " +
+                "\nAja:");
+        int opcion = Integer.parseInt(entrada.readLine());
+
+        if (opcion == 1) { System.out.println(fachada.sacarDinero()); }
+
+        else if (opcion == 2){ System.out.println(fachada.recargaMovil()); }
+
+        else { System.out.println("Ya saben que se van a artar ");}
+
+    }
 }
